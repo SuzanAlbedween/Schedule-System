767be3bbec9a5dd84db0c023f104614d4ea3feb6
abeer <jscy@C25R203-13.campus.ad.bgu.ac.il>
2019-10-16 14:21:01 +0300

    add get_client_product function

--- report_problem.py
+++ report_problem.py
@@ -1,6 +1,20 @@
+import openpyxl
+
 class Problem:
     def report(self):
         print("Enter your id:")
+    def get_client_product(self, client_id):
+        ans = []
+        wb = openpyxl.load_workbook('products.xlsx')
+        sheet1 = wb.get_sheet_by_name('products')
+        for i in range(2, sheet1.max_column + 1):
+            if sheet1.cell(row=i, column=3).value == client_id:
+                ans.append((sheet1.cell(row=i, column=1).value,sheet1.cell(row=i, column=2).value))
+        print(ans)
+
+
+
 
 new_report=Problem()
-new_report.report()
+new_report.reporgitt()
+new_report.get_client_product(123456789)
