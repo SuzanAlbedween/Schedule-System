20ccee2c863acf4c36aed54daae77424e539a89e
nidaa <nida.alk94@gmail.com>
2019-10-16 14:20:24 +0300

    Add method is_client_exist

--- report_problem.py
+++ report_problem.py
@@ -1,6 +1,22 @@
+import openpyxl
+
 class Problem:
     def report(self):
         print("Enter your id:")
 
+    def is_client_exist(self, client_id):
+        client_file = openpyxl.load_workbook('clients.xlsx')
+        client_sheet = client_file.get_sheet_by_name('clients')
+        rows = client_sheet.max_row
+        for i in range (2, rows +1):
+            if(client_sheet.cell(row=i, column=2).value == client_id):
+               return True
+
+        return False
+
+
+
+
 new_report=Problem()
 new_report.report()
+print(new_report.is_client_exist(12355589))
