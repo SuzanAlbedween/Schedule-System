99804fe9bdc8413cbe6c67bd60cf138affb282da
SuzanAlbedween <suzanalwkily@gmail.com>
2019-10-24 11:45:16 +0300

    Updateing the functions

--- report_problem.py
+++ report_problem.py
@@ -1,9 +1,8 @@
 import openpyxl
-<<<<<<< HEAD
+from PyQt5.QtCore import QDate,QTime
+
 class Problem:
-    def report(self):
-        print("Enter your id:")
-    # return  list of problems_types from problems_types file
+
     def get_problems_types(self):
         lsproblems=[]
         f=openpyxl.load_workbook('problems_types.xlsx')
@@ -12,11 +11,18 @@ class Problem:
         for i in range(2,row):
             lsproblems.append(sheet1.cell(row=i, column=2).value)
         return lsproblems
-=======
 
-class Problem:
     def report(self):
-        print("Enter your id:")
+        client_id = input("Enter your id:")
+        try_again_if_wrong=0
+        if self.is_client_exist(client_id):
+            ans=self.get_client_choice(client_id)
+            self.add_problem_to_file([client_id]+ans)
+        else:
+            try_again_if_wrong +=1
+            if try_again_if_wrong == 5:
+                return
+
     def get_client_product(self, client_id):
         ans = []
         wb = openpyxl.load_workbook('products.xlsx')
@@ -24,35 +30,67 @@ class Problem:
         for i in range(2, sheet1.max_column + 1):
             if sheet1.cell(row=i, column=3).value == client_id:
                 ans.append((sheet1.cell(row=i, column=1).value,sheet1.cell(row=i, column=2).value))
-        print(ans)
+        return ans
 
 
-
-
-    def is_client_exist(self, client_id):
+    def is_client_exist( self,client_id):
         client_file = openpyxl.load_workbook('clients.xlsx')
         client_sheet = client_file.get_sheet_by_name('clients')
         rows = client_sheet.max_row
         for i in range (2, rows +1):
             if(client_sheet.cell(row=i, column=2).value == client_id):
                return True
-
         return False
->>>>>>> 3d605e802450d12a6bce31470f501922f7bd8b89
+
+    def get_client_choice(self, id,produc_choice):
+        proud_ls=list(self.get_client_product(id))
+        ans=[]
+        product_file = openpyxl.load_workbook('products.xlsx')
+        product_sheet1 = product_file.get_sheet_by_name('products')
+        for i in proud_ls:
+            if(i==produc_choice):
+                ans.append((product_sheet1.cell(row=i,column=2).value,product_sheet1.cell(row=i,column=3).value))
+                break
+
+        return ans
+
+    def return_problem_type(self, problem_name):
+        wb = openpyxl.load_workbook('problems_types.xlsx')
+        types_sheet = wb['types']
+        rows = types_sheet.max_row
+        for i in range(2, rows + 1):
+            if (types_sheet.cell(row=i, column=2).value == problem_name):
+                return types_sheet.cell(row=i, column=1).value
+
+    def return_product_id(self, product_name, client_id):
+        wb = openpyxl.load_workbook('products.xlsx')
+        product_sheet = wb['products']
+        rows = product_sheet.max_row
+        print(product_name, client_id,rows)
+        for i in range(2, rows + 1):
+            print(product_sheet.cell(row=i, column=2).value, product_name, type( product_sheet.cell(row=i, column=3).value),type(client_id))
+            if (product_sheet.cell(row=i, column=2).value == product_name and
+                    product_sheet.cell(row=i, column=3).value == int(client_id)):
+                d=product_sheet.cell(row=i, column=1).value
+                return d
+
+    def add_problem_to_file(self, product_name, client_id, descpn):
+       ## new_report = Problem()
+        ##typeis = new_report.return_problem_type(descpn)
+        typeis =self.return_problem_type(descpn)
+        product_id = self.return_product_id(product_name, client_id)
+        wb = openpyxl.load_workbook('problems.xlsx')
+        p_sheet = wb[typeis]
+        id = 12345
+        mxrow = p_sheet.max_row
+       # today_date = QDate.currentDate()
+        p_sheet['A' + str(mxrow + 1)] = id + 1
+        p_sheet['B' + str(mxrow + 1)] = client_id
+        p_sheet['C' + str(mxrow + 1)] = product_id
+        print(product_id)
+       # p_sheet['D' + str(mxrow + 1)] = today_date
+        p_sheet['E' + str(mxrow + 1)] = descpn
+        wb.save('problems.xlsx')
 
 
 
-
-new_report=Problem()
-<<<<<<< HEAD
-new_report.report()
-print(new_report.get_problems_types())
-=======
-<<<<<<< HEAD
-new_report.report()
-print(new_report.is_client_exist(12355589))
-=======
-new_report.reporgitt()
-new_report.get_client_product(123456789)
->>>>>>> 767be3bbec9a5dd84db0c023f104614d4ea3feb6
->>>>>>> 3d605e802450d12a6bce31470f501922f7bd8b89
