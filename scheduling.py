072149b369b13493887d57cb14847bd485c63037
nidaa <nida.alk94@gmail.com>
2019-10-29 23:05:12 +0200

    Add new method returning duration of a problem

--- scheduling.py
+++ scheduling.py
@@ -2,3 +2,11 @@ import openpyxl
 import random
 
 class Scheduling:
+
+   def return_problem_duration(self, name_pm):
+        wb = openpyxl.load_workbook('problems_types.xlsx')
+        tp_sheet = wb['types']
+        rows=tp_sheet.max_row
+        for i in range(2, rows + 1):
+            if (tp_sheet.cell(row=i, column=2).value == name_pm):
+                return tp_sheet.cell(row=i, column=3).value
