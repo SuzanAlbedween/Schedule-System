eaad943bbe82927c89fa783729e5e79318bee1bb
nidaa <nida.alk94@gmail.com>
2019-10-30 13:53:15 +0200

    add to scheduling

--- scheduling.py
+++ scheduling.py
@@ -10,3 +10,16 @@ class Scheduling:
         for i in range(2, rows + 1):
             if (tp_sheet.cell(row=i, column=2).value == name_pm):
                 return tp_sheet.cell(row=i, column=3).value
+
+
+   def list_of_lists(self):
+        clientfile = openpyxl.load_workbook('clients.xlsx')
+        clientsheet = clientfile['clients']
+        maxrw = clientsheet.max_row      #maxrow is the number of tech
+        listoflists = []
+        for i in range(2, maxrw+10):
+            sublist=[0,0]
+            listoflists.append(sublist)
+        return listoflists
+
+
