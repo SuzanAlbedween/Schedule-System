0bddc0cff8c403b0a646db4abd5409bc90600e64
abeer <jscy@C25R203-13.campus.ad.bgu.ac.il>
2019-11-21 15:24:48 +0200

    updating the excel file path

--- product.py
+++ product.py
@@ -9,11 +9,16 @@ class Product:
             datetime.date(datetime.now()).year)
 
     def add_product(self, product_name, client_id):
-        products_file = openpyxl.load_workbook('products.xlsx')
+        products_file = openpyxl.load_workbook('excel_files\\products.xlsx')
         products_sheet = products_file['products']
         mxrow = products_sheet.max_row
         products_sheet['A' + str(mxrow + 1)] = self.generate_random_id()  # adding id as str
         products_sheet['B' + str(mxrow + 1)] = product_name
         products_sheet['C' + str(mxrow + 1)] = client_id  # adding id as str
         products_sheet['D' + str(mxrow + 1)] = self.get_date()
-        products_file.save('products.xlsx')
+        products_file.save('excel_files\\products.xlsx')
+
+
+
+p=Product()
+p.add_product("TESeee","3433")
