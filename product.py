f8320f6cfd459d0e340e45caaca81e394713ef39
abeer282 <abeerala282@gmail.com>
2019-11-27 11:49:32 +0200

    update the random id generator

--- product.py
+++ product.py
@@ -1,8 +1,25 @@
 import openpyxl
 from datetime import datetime
+import random
+import string
+
+
 class Product:
+
+    def id_exists(self, product_id):
+        product_file = openpyxl.load_workbook('excel_files\\products.xlsx')
+        product_sheet = product_file['products']
+        p_row = product_sheet.max_row
+        for i in range(2, p_row + 1):
+            if product_sheet.cell(row=i, column=2).value == product_id:
+                return True
+        return False
+
     def generate_random_id(self):
-        return '1111'
+        product_id = str(random.randrange(1000, 9999)) + random.choice(string.ascii_uppercase)
+        if self.id_exists(product_id):
+            self.generate_random_id()
+        return product_id
 
     def get_date(self):
         return str(datetime.date(datetime.now()).day) + "/" + str(datetime.date(datetime.now()).month) + "/" + str(
@@ -11,14 +28,9 @@ class Product:
     def add_product(self, product_name, client_id):
         products_file = openpyxl.load_workbook('excel_files\\products.xlsx')
         products_sheet = products_file['products']
-        mxrow = products_sheet.max_row
-        products_sheet['A' + str(mxrow + 1)] = self.generate_random_id()  # adding id as str
-        products_sheet['B' + str(mxrow + 1)] = product_name
-        products_sheet['C' + str(mxrow + 1)] = client_id  # adding id as str
-        products_sheet['D' + str(mxrow + 1)] = self.get_date()
+        p_row = products_sheet.max_row
+        products_sheet['A' + str(p_row + 1)] = self.generate_random_id()  # adding id as str
+        products_sheet['B' + str(p_row + 1)] = product_name
+        products_sheet['C' + str(p_row + 1)] = client_id  # adding id as str
+        products_sheet['D' + str(p_row + 1)] = self.get_date()
         products_file.save('excel_files\\products.xlsx')
-
-
-
-p=Product()
-p.add_product("TESeee","3433")
