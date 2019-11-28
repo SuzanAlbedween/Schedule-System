8f58c5531aa779cb120f344a620376e729f559f1
SuzanAlbedween <suzanalwkily@gmail.com>
2019-11-28 04:28:25 +0200

    update

--- login_forum.py
+++ login_forum.py
@@ -1,66 +1,46 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'loginforum.ui'
+# Form implementation generated from reading ui file 'loginforum.ui',
+# licensing of 'loginforum.ui' applies.
 #
-# Created by: PyQt5 UI code generator 5.13.0
+# Created: Thu Nov 28 03:17:30 2019
+#      by: pyside2-uic  running on PySide2 5.13.1
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-import openpyxl
-
 class Ui_LoginWindow(object):
-
-
-    def setupUi(self,LoginWindow):
+    def setupUi(self, LoginWindow):
         LoginWindow.setObjectName("LoginWindow")
         LoginWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(LoginWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.username = QtWidgets.QLabel(self.centralwidget)
-        self.username.setGeometry(QtCore.QRect(90, 120, 201, 31))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.username.setFont(font)
-        self.username.setObjectName("username")
-        self.password = QtWidgets.QLabel(self.centralwidget)
-        self.password.setGeometry(QtCore.QRect(100, 260, 151, 21))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.password.setFont(font)
-        self.password.setObjectName("password")
         self.user_name = QtWidgets.QLineEdit(self.centralwidget)
-        self.user_name.setGeometry(QtCore.QRect(90, 160, 271, 41))
+        self.user_name.setGeometry(QtCore.QRect(310, 290, 231, 21))
         self.user_name.setObjectName("user_name")
         self.user_pass = QtWidgets.QLineEdit(self.centralwidget)
-        self.user_pass.setGeometry(QtCore.QRect(100, 310, 271, 41))
+        self.user_pass.setGeometry(QtCore.QRect(310, 350, 231, 21))
         self.user_pass.setText("")
         self.user_pass.setEchoMode(QtWidgets.QLineEdit.Password)
         self.user_pass.setPlaceholderText("")
         self.user_pass.setObjectName("user_pass")
         self.btn_login = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_login.setGeometry(QtCore.QRect(440, 390, 151, 31))
+        self.btn_login.setGeometry(QtCore.QRect(240, 450, 211, 31))
         font = QtGui.QFont()
         font.setFamily("Arial")
         font.setPointSize(12)
         font.setItalic(False)
-        font.setKerning(True)
         self.btn_login.setFont(font)
+        self.btn_login.setAutoFillBackground(False)
         self.btn_login.setObjectName("btn_login")
-        #
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(0, 10, 211, 41))
+        self.label.setGeometry(QtCore.QRect(0, 10, 381, 41))
         font = QtGui.QFont()
         font.setPointSize(20)
-        font.setBold(True)
-        font.setItalic(True)
         font.setWeight(75)
+        font.setItalic(True)
+        font.setBold(True)
         self.label.setFont(font)
         self.label.setObjectName("label")
         LoginWindow.setCentralWidget(self.centralwidget)
@@ -69,15 +49,7 @@ class Ui_LoginWindow(object):
         QtCore.QMetaObject.connectSlotsByName(LoginWindow)
 
     def retranslateUi(self, LoginWindow):
-        _translate = QtCore.QCoreApplication.translate
-        LoginWindow.setWindowTitle(_translate("LoginWindow", "MainWindow"))
-        self.username.setText(_translate("LoginWindow", "Username:"))
-        self.password.setText(_translate("LoginWindow", "Password:"))
-        self.btn_login.setText(_translate("LoginWindow", "Login"))
-        self.label.setText(_translate("LoginWindow", "Welcome"))
+        LoginWindow.setWindowTitle(QtWidgets.QApplication.translate("LoginWindow", "MainWindow", None, -1))
+        self.btn_login.setText(QtWidgets.QApplication.translate("LoginWindow", ".", None, -1))
+        self.label.setText(QtWidgets.QApplication.translate("LoginWindow", "Schedule System", None, -1))
 
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    LoginWindow = QtWidgets.QMainWindow()
