a1ff9d48b1ba8001fe97636721c6ee7678941451
SuzanAlbedween <suzanalwkily@gmail.com>
2019-11-28 14:02:55 +0200

    update thechns classe

--- techns.py
+++ techns.py
@@ -61,4 +61,23 @@ class Techn:
                     return False
                 else:
                     return True
+    def ReturnIDTech(self, username,pwd):
+        tech_file = openpyxl.load_workbook('excel_files\\techns.xlsx')
+        tc_sheet = tech_file['techns']
+        rows = tc_sheet.max_row
+        for i in range(2, rows + 1):
+            if (str(tc_sheet.cell(row=i, column=3).value) == str(username) and str( tc_sheet.cell(row=i, column=4).value) == str(pwd)):
+                return tc_sheet.cell(row=i, column=1).value
+
+
+
+    def ReturnIndexTech(self,id):
+        file = openpyxl.load_workbook('excel_files\\scheduling.xlsx')
+        sheet = file['sheet1']
+        cols = sheet.max_column
+        flage = 0
+        for i in range(1, cols + 1):
+            if (str((sheet.cell(row=1, column=i).value)) == str(id)):
+                flage = i
+        return flage
 
